8ab6858dc69843dd7889dfee06ff848909338f00
LazarusThe3rd <alazar.kebede@student.arteveldehs.be>
2026-06-14 22:49:30 +0200

    ♻️ extract reset password form component

---
+++ full-stack/src/components/Auth/ResetPasswordForm.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { API } from "../../lib/supabaseClient";
+import "../../css/reset.css";
+import "../../css/styleLogin.css";
+
+const ResetPassword = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  useEffect(() => {
+    async function checkSession() {
+      const { data, error } = await API.auth.getSession();
+
+      console.log("SESSION:", data);
+      console.log("ERROR:", error);
+    }
+
+    checkSession();
+  }, []);
+  return (
+    <div className="container">
+      <div className="left">
+        <div className="brand">
+          <span className="waves">≈≈≈</span>
+          <span className="brand-name">Seapark LSS</span>
+        </div>
+
+        <div className="hero-text">
+          <h1>
+            Reset Your
+            <br />
+            Password
+          </h1>
+
+          <p>
+            Enter a new password to regain access to the SeaPark Filtration
+            platform.
+          </p>
+        </div>
+
+        <div className="footer">© Seapark Marine Park</div>
+      </div>
+
+      <div className="right">
+        <div className="login-box">
+          <h2>Reset Password</h2>
+
+          <p className="subtitle">Enter and confirm your new password.</p>
+
+          <form>
+            <div className="form-group">
+              <label>New Password</label>
+
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Confirm Password</label>
+
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+
+            <button type="submit" className="primary-btn">
+              Save Password
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
